a93b81a65b40c6bfefa8002449359e6da1051c47
Shariea Reza Nabil <nabilreza183@gmail.com>
2025-12-15 17:06:12 +0600

    All Done

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import favicon from './assets/Technux.webp'
+
+const link = document.createElement('link')
+link.rel = 'icon'
+link.type = 'image/webp'
+link.href = favicon
+document.head.appendChild(link)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
